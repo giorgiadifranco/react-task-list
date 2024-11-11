2325505e989e9e024146d79d0918baae6da5acfb
giorgiadifranco <giorgiadf08@gmail.com>
2024-11-11 14:53:05 +0100

    fix main

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -1,12 +1,28 @@
 export default function AppMain(){
 
     //logica
-    const list_title = "Current Tasks"
+    const welcome = "Welcome to my React App"
+
+    //logica bonus
+    const list_current = "Current Tasks"
+    const list_completed = "Completed Tasks"
+    
     //markup
     return(
 
         <main>
-            <h2>{list_title}</h2>
+            <h1>{welcome}</h1>
+            <section>
+                <h2>
+                    {list_current}
+                </h2>
+            </section>
+
+            <section>
+                <h2>
+                    {list_completed}
+                </h2>
+            </section>
         </main>
 
     )
